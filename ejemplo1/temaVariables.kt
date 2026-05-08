573257253723b4bbc3187b3a5806487bd4a074b5
Osvaldogms23 <173979043+Osvaldogms23@users.noreply.github.com>
2026-05-07 21:05:31 -0600

    Tipos de datos 07/05/2026

---
+++ ejemplo1/temaVariables.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val saludo: String = "Hola"  // Inmutable
+    var nombre: String = "Kotlin"  // Mutable
+    println ("$saludo $nombre")
+
+    println ("Ingresa primer numero")
+    val num1 = readln().toInt()
+    println (Ingresa segundo numero)
+    val num2 = readln().toInt()
+
+    var res = num1 + num2 
+    println("La suma es: $res")
+}
---
+++ ejemplo2/temaVariablesTipo.kt
@@ -0,0 +1,29 @@
+/*
+Tipo de variable
+
+Byte                           mínimo                 máximo
+                                -128                   +127
+Short                        -32 768                +32 767
+Int                   -2 147 483 648         +2 147 483 647
+Long      -9 223 372 036 854 775 808   +9 223 372 036 854 775 807
+
+Double
+Float
+
+String  " "
+Char    ' '
+
+val valor1 : Int
+val valor2 : Float
+var continuar: Char = 's'
+var fin:Boolean = false
+*/
+
+fun main(){
+    val valor1 = 100
+    val valor2 = 400
+    var res = valor1 + valor2 
+
+    println("La suma de $valor1 + $valor2 = $res")
+
+}
